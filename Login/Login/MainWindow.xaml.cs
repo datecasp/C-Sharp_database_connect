6d87adee250eda548ebcb5c7566c7d1713663830
datecasp <54754720+datecasp@users.noreply.github.com>
2022-04-12 09:40:34 +0200

    v2.0

--- Login/Login/MainWindow.xaml.cs
+++ Login/Login/MainWindow.xaml.cs
@@ -59,40 +59,6 @@ namespace Login
                 {
                     connection.Open();
                     MessageBox.Show(connection.State.ToString());
-
-                    /*****************************************************
-                     * 
-                     *  
-                     *  to check if user credentials are ok, create a SELECT command to use
-                     *  
-                     *  reading the txtBox filled with user´s data
-                     *  
-                     *  With a datareader ask the database.
-                     *  
-                     *  If user filled data matchs a register,
-                     *  
-                     *  the datareader will have rows, 
-                     *  
-                     *  so User/Password are ok and Login is correct
-                     * 
-                     * ****************************************************
-
-                    MySqlCommand cmd = connection.CreateCommand(); 
-                    cmd.CommandText = "SELECT user, pwd FROM tec.credenciales WHERE user='"+txtUserIn.Text+
-                        "' AND pwd='"+txtPwdIn.Password.ToString()+"';";
-
-                    MySqlDataReader dr = cmd.ExecuteReader();
-
-                    if (dr.HasRows)
-                    {
-                        MessageBox.Show("LOGIN OK");
-                    }
-                    else
-                    {
-                        MessageBox.Show("WRONG USER/PASSWORD");
-                    }
-                    */
-
                     connection.Close(); 
                 }
             }
