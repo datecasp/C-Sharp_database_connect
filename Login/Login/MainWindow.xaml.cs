7ead2d0e43e0fca181ee7c17a1db3782f55ee0a9
datecasp <54754720+datecasp@users.noreply.github.com>
2022-04-13 00:39:58 +0200

    Upgrade
    Addeed funcionality to manage database

--- Login/Login/MainWindow.xaml.cs
+++ Login/Login/MainWindow.xaml.cs
@@ -22,20 +22,8 @@ namespace Login
     /// </summary>
     public partial class MainWindow : Window
     {
+        MySqlConnection connection = null;
 
-<<<<<<< HEAD
-
-
-        /********************************************************************************
-         * 
-         * https://stackoverflow.com/questions/29729658/wpf-open-new-window-in-wpf
-         * 
-         * Para sustituir UI de login por UI de herramientas SQL
-         * 
-         * ******************************************************************************/
-
-=======
->>>>>>> a62690b1b3689de145a2b26cb3ca8465d958d744
         public MainWindow()
         {
             InitializeComponent();
@@ -56,17 +44,13 @@ namespace Login
                 builder.UserID = txtUserIn.Text;
                 builder.Password = txtPwdIn.Password;
 
-                using (MySqlConnection connection = new(builder.ConnectionString))
-                {
-                    connection.Open();
-                    MessageBox.Show(connection.State.ToString());
-
-                    //CAMBIO DE INTERFAZ MAIN A HERRAMIENTAS SQL
-
-                    grdMain.Visibility = System.Windows.Visibility.Collapsed;
-                    grdTools.Visibility = System.Windows.Visibility.Visible;
-                    connection.Close(); 
-                }
+                connection = new(builder.ConnectionString);
+                
+                connection.Open();
+                grdMain.Visibility = System.Windows.Visibility.Collapsed;
+                grdTools.Visibility = System.Windows.Visibility.Visible;
+                    
+                
             }
             catch (SqlException ex)
             {
@@ -74,5 +58,107 @@ namespace Login
             }
         
     }
+
+        //TOOLS UI REGION
+        private void BtnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            grdTools.Visibility = System.Windows.Visibility.Collapsed;
+            grdInsert.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void BtnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            grdTools.Visibility=System.Windows.Visibility.Collapsed;
+            stkSelect.Visibility = System.Windows.Visibility.Visible;  
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            grdTools.Visibility=System.Windows.Visibility.Collapsed;
+            stkDelete.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Close();
+            this.Close();
+        }
+
+
+        //INSERT
+        private void BtnInsertData_Click(object sender, RoutedEventArgs e)  
+        {
+            //MessageBox.Show(connection.State.ToString());
+            try 
+            { 
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO tec.credenciales (user, pwd) VALUES ('" + txtUserInsert.Text +
+                    "','" + txtPassInsert.Text + "');";
+                //MessageBox.Show(cmd.CommandText.ToString());
+
+                cmd.ExecuteReader();
+                MessageBox.Show("INSERT DONE");
+            }
+            catch (Exception ex)    
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        //DELETE
+        private void BtnDeleteData_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "DELETE FROM tec.credenciales WHERE user='" + txtUserDelete.Text +
+                    "';";
+                //MessageBox.Show(cmd.CommandText.ToString());
+
+                cmd.ExecuteReader();
+                MessageBox.Show("DELETE DONE");
+            }   
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //SELECT
+        private void BtnSelectData_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT user, pwd FROM tec.credenciales WHERE user='" + txtUserSelect.Text +
+                    "';";
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    MessageBox.Show("USER ="+dr.GetString(0) +" PWD =" +dr.GetString(1));
+                }
+                else
+                {
+                    MessageBox.Show("USER NOT FOUND");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //BACK
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            grdTools.Visibility = System.Windows.Visibility.Visible;
+            grdInsert.Visibility = System.Windows.Visibility.Collapsed;
+            stkDelete.Visibility = System.Windows.Visibility.Collapsed;
+            stkSelect.Visibility = System.Windows.Visibility.Collapsed;
+        }
     }
 }
