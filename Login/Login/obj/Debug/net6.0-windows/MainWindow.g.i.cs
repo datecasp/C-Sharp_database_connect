58f9dc0e5ab61a3c45fe2eaacbcf12c288a6c914
datecasp <54754720+datecasp@users.noreply.github.com>
2022-04-12 10:04:32 +0200

    Create app.config
    create app.config to hold definitions and preferences

--- Login/Login/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Login/Login/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace Login {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Login;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Login;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
